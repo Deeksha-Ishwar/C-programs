2f70cd1a8248ad43bb96a0c67b7ca58218c96e3c
Deeksha-Ishwar <deekshaishwar8@gmail.com>
2024-12-12 20:24:52 +0530

    multiplication tables of user enetere number

---
+++ multiplication_tables.c
@@ -0,0 +1,12 @@
+//multiplication tables of user entered number
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num,i;
+    printf("Enter a number:- ");
+    scanf("%d",&num);
+    for(i=1;i<=10;i++){
+        printf("%d x %d = %d\n",num,i,num*i);
+    }
+    return 0; 
+}
